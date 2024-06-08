a5f6c444787632a2aee3e0fe8a678d7f84c93ce2
Dmytro Shvetsov <dmytro.shvetsov@feld.ai>
2024-06-08 09:53:05 +0200

    feat: listen to mouse click
    - add event listeners for `mousedown` and `mouseup`
    - remove listeners on unmount

--- src/useKeys.js
+++ src/useKeys.js
@@ -2,24 +2,46 @@ import { useEffect } from 'react';
 
 export const useKeys = (handleStart, handleStop) => {
   const keyDownHandler = (e) => {
+    console.log(e);
     if (e.key === 'Shift') {
       handleStart();
     }
-  }
+  };
 
   const keyUpHandler = (e) => {
+    console.log(e);
     if (e.key === 'Shift') {
       handleStop();
     }
-  }
+  };
+
+  const mouseDownHandler = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    console.log(e);
+    handleStart();
+  };
+
+  const mouseUpHandler = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    console.log(e);
+    handleStop();
+  };
 
   useEffect(() => {
     window.addEventListener('keydown', keyDownHandler);
     window.addEventListener('keyup', keyUpHandler);
 
+    window.addEventListener('mousedown', mouseDownHandler);
+    window.addEventListener('mouseup', mouseUpHandler);
+
     return () => {
       window.removeEventListener('keydown', keyDownHandler);
       window.removeEventListener('keyup', keyUpHandler);
+
+      window.removeEventListener('mousedown', mouseDownHandler);
+      window.removeEventListener('mouseup', mouseUpHandler);
     };
   }, []);
 };
