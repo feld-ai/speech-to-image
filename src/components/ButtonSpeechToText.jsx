24e4fef58f0075226a00bce4d15b651307d21987
Dmytro Shvetsov <dmytro.shvetsov@feld.ai>
2024-06-08 12:16:55 +0200

    chore: adjust condition for yt

--- src/components/ButtonSpeechToText.jsx
+++ src/components/ButtonSpeechToText.jsx
@@ -105,7 +105,7 @@ export const ButtonSpeechToText = () => {
         </div>
         <div>
           {yt.description && <p>{yt.description}</p>}
-          {yt.link && (
+          {yt.link && !loading && (
             <YoutubeEmbed link={yt.link} />
           )}
         </div>
